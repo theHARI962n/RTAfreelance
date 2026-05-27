a288c162519000e6fc986da8f3ac7fe9a26bc2d8
theHARI962n <rajul1874@gmail.com>
2026-05-27 20:43:05 +0530

    modified testimonial section

--- src/components/Landingpage/TestimonialSection.jsx
+++ src/components/Landingpage/TestimonialSection.jsx
@@ -6,13 +6,13 @@ export default function TestimonialsSection() {
     ];
   
     return (
-      <section id="testimonials" className="py-28 px-6 bg-white mt-20 md:mt-0">
+      <section id="testimonials" className="py-28 px-6 bg-[#fffef9] mt-20 md:mt-0">
         <h3 className="text-4xl font-extrabold text-center mb-16">
           What Our <span className="text-pink-600">Students Say</span>
         </h3>
         <div className="grid md:grid-cols-3 gap-8 max-w-6xl mx-auto">
           {testimonials.map((t, i) => (
-            <div key={i} className="bg-gray-50 shadow p-6 rounded-xl text-center hover:shadow-lg transition">
+            <div key={i} className="bg-[#faf8f3] shadow p-6 rounded-xl text-center hover:shadow-lg transition">
               <p className="text-gray-600 italic mb-4">“{t.feedback}”</p>
               <h5 className="font-semibold text-gray-900">— {t.name}</h5>
             </div>
